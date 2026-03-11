8abffae4fc1c9907d94c0f258bd26aa28d3a65e0
Michal Knizek <mknizek@gmail.com>
2026-03-11 14:03:33 +0100

    Show helpful error when run without Bun

--- bin/macroclaw.js
+++ bin/macroclaw.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env bun
+if (typeof Bun === "undefined") {
+  console.error("macroclaw requires Bun. Install it: https://bun.sh");
+  process.exit(1);
+}
 await import("../src/index.ts");
